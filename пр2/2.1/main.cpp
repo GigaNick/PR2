52176c88777e8306af7320d4cb1a7cba81b42e20
GigaNick <125920908+GigaNick@users.noreply.github.com>
2023-02-26 20:06:19 +0300

    Add files via upload

---
+++ пр2/2.1/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "triangle.h"
+#include <cmath>
+#include <iostream>
+int main()
+{
+    Triangle mas[3];
+    double a, b, c;
+    for (int i = 0; i < 3; i++) {
+        cout << "введите a, b, c для треугольника N " << i + 1 << " через пробел: " << endl;
+        cin >> a >> b >> c;
+        mas[i].set(a, b, c);
+        if (!(mas[i].exst_tr())) {
+            mas[i].show();
+            cout << "Треугольника с такими сторонами не существует, попробуйте ещё раз" << endl;
+            i--;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        mas[i].show();
+        cout << "Периметр треугольника: " << mas[i].perimetr() << endl;
+        cout << "Площадь треугольника: " << mas[i].square() << endl;
+    }
+    return 0;
+}
---
+++ пр2/2.1/triangle.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Triangle {
+private:
+    double a, b, c;
+
+public:
+    bool exst_tr();
+
+    void set(double a1, double b1, double c1);
+
+    void show();
+
+    double perimetr();
+
+    double square();
+};
